b6be2cb7af20e06e9711f350846bd26b0d416db1
aleksandrkosarev <nort3d@ya.ru>
2021-03-03 17:15:46 +0300

    Create custom cell

---
+++ yandex/Models/CompanyShortInfoModel.swift
@@ -0,0 +1,19 @@
+//
+//  CompanyShortInfoModel.swift
+//  yandex
+//
+//  Created by Александр Косарев on 03.03.2021.
+//
+
+import UIKit
+
+struct CompanyShortInfo {
+    let label: String
+    let name: String
+    let icon: UIImage
+    let price: Float
+    let currencyType: String
+    let priceDynamic: Float
+    let proceDynamicPercent: Float
+    var isFaforite: Bool
+}
